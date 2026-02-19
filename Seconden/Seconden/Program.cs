530c4b10ae143da494e0afdcb7f582512a1ca105
RobbeRooijakkersPXL <robberooijakkers@student.pxl.be>
2026-02-19 15:14:33 +0100

    Seconden omvormen naar Uur, minuut en second

---
+++ Seconden/Seconden/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Seconden
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Geef het aantal seconden");
+            string input = Console.ReadLine();
+
+            if(int.TryParse(input, out int totaalSeconden))
+            {
+                int uren = totaalSeconden / 3600;
+                int minuten = (totaalSeconden % 3600) / 60;
+                int seconden = totaalSeconden % 60;
+
+                Console.WriteLine($"H:{uren} M:{minuten} S:{seconden}");
+            }
+            else
+            {
+                Console.WriteLine("Dit is geen nummer...");
+            }
+        }
+    }
+}
