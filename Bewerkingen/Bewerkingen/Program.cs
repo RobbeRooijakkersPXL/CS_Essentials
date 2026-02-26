b85032a5c6dffa53b8d3e789d363be9f343565a0
RobbeRooijakkersPXL <robberooijakkers@student.pxl.be>
2026-02-26 14:09:54 +0100

    double ipv int, Console.WriteLine op betere plek. Return op default

--- Bewerkingen/Bewerkingen/Program.cs
+++ Bewerkingen/Bewerkingen/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            int som = 0;
+            double som = 0;
 
             Console.Write("Getal 1: ");
             if(!int.TryParse(Console.ReadLine(), out int number1)) // EXAMEN/PE VRAAG!!!
@@ -25,27 +25,27 @@
             switch(operatorSign)
             {
                 case "+":
-                    som = number1 + number2;
-                    Console.WriteLine($"Resultaat: {som}");
+                    som = number1 + (double)number2;
                     break;
             
             
                 case "-":
-                    som = number1 - number2;
-                    Console.WriteLine($"Resultaat: {som}");
+                    som = number1 - (double)number2;
                     break;
 
                 case "/":
-                    som = number1 / number2;
-                    Console.WriteLine($"Resultaat: {som}");
+                    som = number1 / (double)number2;
                     break;
 
                 case "*":
-                    som = number1 * number2;
-                    Console.WriteLine($"Resultaat: {som}");
+                    som = number1 * (double)number2;
                     break;
 
+                default:
+                    Console.WriteLine("Bewerking niet ondersteund");
+                    return;
             }
+            Console.WriteLine($"Resultaat: {som:F2}");
 
         }
     }
