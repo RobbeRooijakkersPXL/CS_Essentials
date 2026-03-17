e5a08e3ba4c2eef5244c126e703c78898b573ae4
RobbeRooijakkersPXL <robberooijakkers@student.pxl.be>
2026-03-17 11:31:30 +0100

    WOPPA

--- EscapeRoom2/EscapeRoom2/Program.cs
+++ EscapeRoom2/EscapeRoom2/Program.cs
@@ -4,20 +4,84 @@
     {
         static void Main(string[] args)
         {
+            StartGame();
+
+            
+        }
+        private static void StartGame()
+        {
             int minutesLeft = 15;
             bool isKeypadSolved = false;
             bool isRiddleSolved = false;
+            bool escaped = false;
+            bool gaveUp = false;
 
-            ShowTitle();
-            ShowIntro();
-            ShowStatus(minutesLeft, isKeypadSolved, isRiddleSolved);
-            ShowMenu();
-            int action = ReadAction();
 
-            if(action == 1)
+            do
             {
-                TryKeypad();
-            }
+                ShowTitle();
+                ShowIntro();
+                ShowStatus(minutesLeft, isKeypadSolved, isRiddleSolved);
+                ShowMenu();
+                int action = ReadAction();
+
+                switch (action)
+                {
+                    case 1:
+                        if(isKeypadSolved == true)
+                        {
+                            ShowColoredText("The Keypad is already solved!", ConsoleColor.Green, true);
+                        }
+                        else
+                        {
+                            isKeypadSolved = TryKeypad();
+                            if(isKeypadSolved == false)
+                            {
+                                minutesLeft = ApplyPenalty(minutesLeft, 2);
+                            }
+                        }
+
+                        
+                        break;
+
+                    case 2:
+                        if(isRiddleSolved == true)
+                        {
+                            ShowColoredText("The Riddle was already solved!", ConsoleColor.Green, true);
+                        }
+                        else
+                        {
+                            isRiddleSolved = SolveRiddle();
+                            if(isRiddleSolved == false)
+                            {
+                                minutesLeft = ApplyPenalty(minutesLeft, 2);
+                            }
+                        }
+                        
+                        break;
+
+                    case 3:
+                        escaped = FinalDoor(isKeypadSolved, isRiddleSolved);
+                        if (!escaped)
+                        {
+                            minutesLeft = ApplyPenalty(minutesLeft, 1);
+                        }
+
+                        break;
+
+                    case 9:
+                        gaveUp = true;
+                        break;
+
+                    default:
+                        minutesLeft = ApplyPenalty(minutesLeft, 2);
+                        break;
+
+                }
+
+                
+                Console.ReadLine();
+            } while (!escaped && !gaveUp);
         }
 
         private static void ShowTitle()
@@ -86,7 +150,7 @@
             {
                 newTime = 0;
             }
-            ShowColoredText($"Time penalty: -{penalty} minute(s).", ConsoleColor.DarkYellow, true);
+            ShowColoredText($"Time penalty: -{penalty} minute(s).", ConsoleColor.DarkYellow, false);
             return newTime;
         }
 
@@ -113,6 +177,58 @@
             }
 
             
+        }
+
+        private static bool SolveRiddle()
+        {
+            ShowColoredText("Riddle: Speak friend and enter...", ConsoleColor.Magenta, true);
+            ShowColoredText("Type the secret word: ", ConsoleColor.Magenta, true);
+
+            string answer = Console.ReadLine();
+
+            if(answer.Equals("open-sesame", StringComparison.OrdinalIgnoreCase))
+            {
+                ShowColoredText("The wall slides aside", ConsoleColor.Green, true);
+                return true;
+            }
+            else
+            {
+                ShowColoredText("The wall remains silend", ConsoleColor.Red, true);
+                return false;
+            }
+        }
+        
+        private static bool FinalDoor(bool keypadSolved, bool riddleSolved)
+        {
+            //TODO: Controleer keypadSolved en riddleSolved
+            if(!keypadSolved || !riddleSolved)
+            {
+                ShowColoredText("The final door won’t budge. Solve the other puzzles first.", ConsoleColor.Red, true);
+                return false;
+            }
+
+
+            ShowColoredText("Final number (1..5): ", ConsoleColor.Magenta, false);
+            string input = Console.ReadLine();
+
+            //TODO: Vul verder aan
+
+            if (input.Equals("3"))
+            {
+                ShowColoredText("You hear the lock click!", ConsoleColor.Green, true);
+                return true;
+
+            }
+            else if (!int.TryParse(input, out _))
+            {
+                ShowColoredText("That is not between 1 and 5.", ConsoleColor.DarkRed, true);
+                return false;
+            }
+            else
+            {
+                ShowColoredText("A buzzer sounds. Wrong!", ConsoleColor.DarkRed, true);
+                return false;
+            }
         }
     }
 }
