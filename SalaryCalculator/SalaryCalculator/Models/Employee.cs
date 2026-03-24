47f5536b3dd2f0cee404fc6679ed675549a94a1d
RobbeRooijakkersPXL <robberooijakkers@student.pxl.be>
2026-03-24 10:23:18 +0100

    Eerste oefening met klasse

---
+++ SalaryCalculator/SalaryCalculator/Models/Employee.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryCalculator.Models
+{
+    public class Employee
+    {
+        public Employee(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public Employee()
+        {
+
+        }
+
+
+        private string _firstName;
+        private string _lastName;
+        private DateTime _birthDate;
+        private decimal _salary;
+        
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+
+        public DateTime BirthDate
+        {
+            get { return _birthDate; }
+            set { _birthDate = value; }
+        }
+
+        public decimal Salary
+        {
+            get { return _salary; }
+            set { _salary = value; }
+        }
+
+        public int Age
+        {
+            get { return DateTime.Now.Year - BirthDate.Year; }
+        }
+
+        public void IncreaseSalary(int percentage)
+        {
+            
+            Salary += (Salary * percentage/100M);
+
+
+        }
+    }
+}
---
+++ SalaryCalculator/SalaryCalculator/Program.cs
@@ -0,0 +1,26 @@
+﻿using SalaryCalculator.Models;
+
+namespace SalaryCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Employee employee = new Employee();
+
+            Console.Write($"Voornaam: ");
+            employee.FirstName = Console.ReadLine();
+
+            Console.Write($"Achternaam: ");
+            employee.LastName = Console.ReadLine();
+
+            Console.Write("Wat is je huidig salaris: ");
+            employee.Salary = decimal.Parse(Console.ReadLine());
+
+            employee.IncreaseSalary(10);
+            Console.WriteLine($"Nieuwe salaris: {employee.Salary:c}");
+        }
+    }
+}
