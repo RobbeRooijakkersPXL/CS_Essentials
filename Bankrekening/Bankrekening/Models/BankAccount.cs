c839149be648447a45def3c06f620c1eb3f64142
RobbeRooijakkersPXL <robberooijakkers@student.pxl.be>
2026-04-02 14:26:32 +0200

    Bankrekening

---
+++ Bankrekening/Bankrekening/Models/BankAccount.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bankrekening.Models
+{
+    public class BankAccount
+    {
+		private decimal _currentBalance;
+
+		public decimal CurrentBalance
+		{
+			get { return _currentBalance; }
+			
+		}
+
+		public BankAccount()
+		{
+			_currentBalance = 50;
+		}
+
+		public void Deposit(decimal amount) 
+		{
+			if(amount > 0)
+			{
+				_currentBalance += amount;
+			}
+		}
+
+		public void Withdraw(decimal amount) 
+		{ 
+			if(amount > 0)
+			{
+                if (amount > _currentBalance)
+                {
+					amount = _currentBalance;
+				}
+
+            }
+
+			_currentBalance -= amount;
+
+		}
+	}
+}
---
+++ Bankrekening/Bankrekening/Program.cs
@@ -0,0 +1,74 @@
+﻿using Bankrekening.Models;
+namespace Bankrekening
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Bankrekening");
+            Console.ResetColor();
+
+            string input;
+            decimal amount;
+            
+            BankAccount bankaccount = new BankAccount();
+            ShowBalance(bankaccount);
+
+
+            do 
+            {
+                
+                do
+                {
+                    Console.Write("Geef het bedrag (+ voor storting, - voor afhaling): ");
+                    input = Console.ReadLine();
+
+                    if (input.Equals("STOP", StringComparison.OrdinalIgnoreCase))
+                    {
+                        return;
+                    }
+
+                } while (!decimal.TryParse(input, out amount));
+
+                if (amount > 0)
+                {
+                    IncreaseAmount(bankaccount, amount);
+                    ShowBalance(bankaccount);
+
+
+                }
+                else if (amount < 0)
+                {
+                    
+                    DecreaseAmount(bankaccount, amount);
+                    ShowBalance(bankaccount);
+
+                }
+
+
+            } while (true);
+            
+        }
+
+        static void IncreaseAmount(BankAccount bankaccount,decimal amount) 
+        {
+            bankaccount.Deposit(amount);
+        
+        }
+
+        static void DecreaseAmount(BankAccount bankaccount, decimal amount) 
+        {
+            bankaccount.Withdraw(amount);
+        }
+
+        static void ShowBalance(BankAccount bankaccount)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"Saldo: {bankaccount.CurrentBalance:c} ");
+            Console.ResetColor();        
+        }
+    }
+}
