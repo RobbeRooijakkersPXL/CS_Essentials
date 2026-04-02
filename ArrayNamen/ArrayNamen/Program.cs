ef1515c9acd481460cce02798d63a7aee1cab392
RobbeRooijakkersPXL <robberooijakkers@student.pxl.be>
2026-04-02 14:26:57 +0200

    1e oefening met Arrays

---
+++ ArrayNamen/ArrayNamen/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ArrayNamen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] name = new string[6];
+
+            Console.WriteLine("Geef de namen van je lectoren in: ");
+
+            for (int i = 0; i < name.Length; i++)
+            {
+                Console.Write($"{i+1}: ");
+                name[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine();
+
+            foreach(string element in name)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
