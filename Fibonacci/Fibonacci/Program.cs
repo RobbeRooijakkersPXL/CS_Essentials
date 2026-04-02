a6c16de5f3495cd6b27ab1621d67a59f21ece069
RobbeRooijakkersPXL <robberooijakkers@student.pxl.be>
2026-04-02 15:02:27 +0200

    beterderrr

--- Fibonacci/Fibonacci/Program.cs
+++ Fibonacci/Fibonacci/Program.cs
@@ -8,9 +8,11 @@
             fibonacci[0] = 0;
             fibonacci[1] = 1;
 
-            for (int i = 0; i < 10; i++)
+            for (int i = 2; i < 12; i++)
             {
-                fibonacci[i + 2] = fibonacci[0 + i] + fibonacci[1 + i];
+                //fibonacci[i + 2] = fibonacci[0 + i] + fibonacci[1 + i];
+
+                fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
             }
 
             foreach(int element in fibonacci)
---
+++ Priemgetallen/Priemgetallen/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Priemgetallen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] primeNumbers = new int[]; 
+        }
+    }
+}
