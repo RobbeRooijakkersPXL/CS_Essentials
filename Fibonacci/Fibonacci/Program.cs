dc72d192156c65102ba7ca14af9e45c79a6342bb
RobbeRooijakkersPXL <robberooijakkers@student.pxl.be>
2026-04-02 14:54:56 +0200

    Woppa

---
+++ Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] fibonacci = new int[12];
+            fibonacci[0] = 0;
+            fibonacci[1] = 1;
+
+            for (int i = 0; i < 10; i++)
+            {
+                fibonacci[i + 2] = fibonacci[0 + i] + fibonacci[1 + i];
+            }
+
+            foreach(int element in fibonacci)
+            {
+                Console.WriteLine(element);
+            }
+
+        }
+    }
+}
